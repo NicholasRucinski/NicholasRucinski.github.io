f01f086f91344fbcfdcd6074a947b7222ba6c8b3
Nick Rucinski <nrucinski01@gmail.com>
2023-09-13 10:34:56 -0400

    testing mean stack

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeFormComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
