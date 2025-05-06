9a6b123ea9facc1cf33692ef8f98dfb8a03905c1
Nicholas Rucinski <tur45021@temple.edu>
2025-05-06 19:51:01 -0400

    updated vite config to copy the cname file on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'CNAME',
+          dest: '.',
+        },
+      ],
+    }),
     react(),
     tailwindcss()
   ],
