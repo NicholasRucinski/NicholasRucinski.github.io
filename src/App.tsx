b20fc88ff5ac5dde4551e80a19cb04ac09d39870
Nicholas Rucinski <nrucinski01@gmail.com>
2025-09-09 14:17:36 -0400

    added a games section

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,46 @@ import ProjectNotFound from "./pages/ProjectNotFound";
 import Servers from "./pages/Servers";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route element={<RootLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/:projectName" element={<ProjectDetails />} />
-          <Route path="/not-found" element={<ProjectNotFound />} />
-          <Route path="/minecraft" element={<Servers />} />
-        </Route>
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route element={<RootLayout />}>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/projects/:projectName" element={<ProjectDetails />} />
+					<Route path="/not-found" element={<ProjectNotFound />} />
+					<Route path="/minecraft" element={<Servers />} />
+					<Route path="/games" element={<Games />} />
+				</Route>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
 
 const RootLayout = () => {
-  return (
-    <div className="">
-      <Nav />
-      <div className="text-foreground mx-auto w-[750px] max-w-full px-5 pt-28 pb-10">
-        <Outlet />
-      </div>
-    </div>
-  );
+	return (
+		<div className="">
+			<Nav />
+			<div className="text-foreground mx-auto w-[750px] max-w-full px-5 pt-28 pb-10">
+				<Outlet />
+			</div>
+		</div>
+	);
+};
+
+const Games = () => {
+	return (
+		<div style={{ width: "800px", height: "600px" }}>
+			<iframe
+				src="/games/particles/Particles.html"
+				width="100%"
+				height="100%"
+				allowFullScreen
+				title="My Game"
+			/>
+		</div>
+	);
 };
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,32 +3,33 @@ import { Link, useLocation } from "react-router-dom";
 import { ThemeSwitcher } from "./theme-switcher";
 
 export default function Nav() {
-  const location = useLocation();
-  const links = [
-    { path: "/", text: "Home" },
-    { path: "/about", text: "About" },
-    { path: "/projects", text: "Projects" },
-  ];
+	const location = useLocation();
+	const links = [
+		{ path: "/", text: "Home" },
+		{ path: "/about", text: "About" },
+		{ path: "/games", text: "Games" },
+		{ path: "/projects", text: "Projects" },
+	];
 
-  return (
-    <div className="fixed top-5 left-0 z-50 w-full">
-      <nav className="text-main-foreground border-border shadow-shadow rounded-base bg-main font-base w450:gap-4 mx-auto flex w-max gap-5 border-2 p-2.5 px-5 text-sm sm:text-base">
-        {links.map((link) => (
-          <Link
-            key={link.path}
-            to={link.path}
-            className={clsx(
-              "hover:border-border rounded-base border-2 px-2 py-1 transition-colors",
-              location.pathname === link.path
-                ? "border-border"
-                : "border-transparent"
-            )}
-          >
-            {link.text}
-          </Link>
-        ))}
-        <ThemeSwitcher />
-      </nav>
-    </div>
-  );
+	return (
+		<div className="fixed top-5 left-0 z-50 w-full">
+			<nav className="text-main-foreground border-border shadow-shadow rounded-base bg-main font-base w450:gap-4 mx-auto flex w-max gap-5 border-2 p-2.5 px-5 text-sm sm:text-base">
+				{links.map((link) => (
+					<Link
+						key={link.path}
+						to={link.path}
+						className={clsx(
+							"hover:border-border rounded-base border-2 px-2 py-1 transition-colors",
+							location.pathname === link.path
+								? "border-border"
+								: "border-transparent"
+						)}
+					>
+						{link.text}
+					</Link>
+				))}
+				<ThemeSwitcher />
+			</nav>
+		</div>
+	);
 }
